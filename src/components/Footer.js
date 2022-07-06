8ff5ca7bbf5121697d57483306c536cfe9a7d22e
Samuel Han <60247381+Sammy713@users.noreply.github.com>
2022-07-06 14:39:55 +0900

    Made aboutme responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,10 @@ function Footer() {
             <div className="footerContainer">
                 <div className='copyright'>© 2022 Copyright: Samuel Han</div>
                 <div className='vert'>|</div>
-                <hr/>
+                <hr className='hr'/>
                 <div className='footerEmail'><img id='email' src='https://tompfister.files.wordpress.com/2014/06/white-email-icon-png-u1nhol7t.png?w=600' /> samuel.han@stonybrook.edu</div>
                 <div className='vert'>|</div>
-                <hr/>
+                <hr className='hr'/>
                 <div className='footerPhone'><img id='phone' src='https://icon-library.com/images/call-icon-vector/call-icon-vector-1.jpg' />010-5803-8211</div>
             </div>
         </div>
